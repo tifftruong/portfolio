4867e065a9331cfca1cb6ead854cd6fc93f9234b
Tiffany <tiff21@uw.edu>
2020-02-12 10:16:48 -0800

    Updates

---
+++ docs/precache-manifest.aaf39c7701d5d782d9270dc91755acd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e72aeee876ecdf076bab6475749331",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3d1b3815a1dd71302a4f",
+    "url": "/portfolio/static/css/main.3bd372b9.chunk.css"
+  },
+  {
+    "revision": "f686e99a9b6a6c927b70",
+    "url": "/portfolio/static/js/2.4afbc96e.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/portfolio/static/js/2.4afbc96e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1b3815a1dd71302a4f",
+    "url": "/portfolio/static/js/main.f0ad0249.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "4317be933b8b615a4b7847d40093ae3f",
+    "url": "/portfolio/static/media/header.4317be93.jpg"
+  }
+]);
